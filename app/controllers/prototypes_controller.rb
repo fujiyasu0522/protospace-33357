52d20a4aef320dcde303b9c9c1183f7739fe6656
fujiyasu0522 <522yasuaki@gmail.com>
2021-04-18 17:02:40 +0900

    新規投稿エラーの対応

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,7 @@
 class PrototypesController < ApplicationController
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_into_index, except: [:index, :show, :new]
+  before_action :move_into_index, except: [:index, :show, :new, :create, :update]
   def index
     @prototypes = Prototype.includes(:user)
   end
